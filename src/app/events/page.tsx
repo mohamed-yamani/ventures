edfcaa44afe4412a051214cc63088673ddf620d5
Mohamed Yamani <myamani@Mohameds-MacBook-Pro-2.local>
2024-09-18 16:31:39 +0100

    news

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -28,7 +28,7 @@ const images: { src: string; link: string; date: string; title: string }[] = [
 export default function Portfolio() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start overflow-hidden">
-      <div className="relative h-[40rem] w-full items-center justify-center">
+      <div className="relative h-[30rem] w-full items-center justify-center">
         <div className="absolute h-full w-full flex-col items-center justify-center gap-4">
           <Image
             src="/assets/gallary/img2.png"
@@ -47,42 +47,55 @@ export default function Portfolio() {
       <div className="container flex w-full flex-col items-start justify-center gap-4 pb-16">
         <div className="gap-2sm:grid-cols-2 grid w-full grid-cols-1 gap-2 pt-32 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
           {images.map(({ src, date, title, link }, index) => (
-            <div
-              key={index}
-              className="group relative flex h-96 cursor-pointer flex-wrap items-start justify-start rounded-none bg-gray-100 transition-all duration-300"
-            >
-              <div className="relative h-3/6 w-full">
-                <Image
-                  src={src}
-                  alt={`Company ${index + 1}`}
-                  layout="fill"
-                  objectFit="cover"
-                  className="group-hover:brightness transition-all duration-300"
-                />
-              </div>
-              <Button className="absolute translate-x-6 translate-y-6 rounded-none bg-secondary font-light text-primary text-white transition duration-300 ease-in-out hover:bg-white hover:text-primary">
-                ANNUAL MEETING SIDE EVENT
-              </Button>
-
-              <p className="text-grey bottom-0 left-0 bg-opacity-90 px-6 pt-2">
-                {date}
-              </p>
-
-              <p className="text-grey bottom-0 right-0 bg-opacity-90 px-6 text-2xl text-primary">
-                {title}
-              </p>
-
-              <div className="flex flex-row items-center justify-center gap-6 bg-opacity-90 px-6 py-4">
-                <p className="text-grey text-lg text-secondary">
-                  {/* read more */}
-                  READ MORE
-                </p>
-                <CircleArrowRight className="text-secondary" />
-              </div>
-            </div>
+            <EventsCard src={src} date={date} title={title} key={index} />
           ))}
         </div>
       </div>
     </main>
+  );
+}
+
+interface EventsCardProps {
+  src: string;
+  date: string;
+  title: string;
+  key: number;
+}
+
+function EventsCard({ src, date, title, key }: EventsCardProps) {
+  return (
+    <div
+      key={key}
+      className="group relative flex h-96 cursor-pointer flex-wrap items-start justify-start rounded-none bg-gray-100 transition-all duration-300"
+    >
+      <div className="relative h-3/6 w-full">
+        <Image
+          src={src}
+          alt={`Company ${key + 1}`}
+          layout="fill"
+          objectFit="cover"
+          className="group-hover:brightness transition-all duration-300"
+        />
+      </div>
+      <div className="absolute translate-x-6 translate-y-6 rounded-none bg-secondary px-4 py-2 font-light text-primary text-white transition duration-300 ease-in-out">
+        ANNUAL MEETING SIDE EVENT
+      </div>
+
+      <p className="text-grey bottom-0 left-0 bg-opacity-90 px-6 pt-2">
+        {date}
+      </p>
+
+      <p className="text-grey bottom-0 right-0 bg-opacity-90 px-6 text-2xl text-primary">
+        {title}
+      </p>
+
+      <div className="flex flex-row items-center justify-center gap-6 bg-opacity-90 px-6 py-4">
+        <p className="text-grey text-lg text-secondary">
+          {/* read more */}
+          READ MORE
+        </p>
+        <CircleArrowRight className="text-secondary" />
+      </div>
+    </div>
   );
 }
